b372ae2a58c653a8292dffa9babaa20dc5950709
frontdev24kv <kv.frontdev24@gmail.com>
2025-03-07 21:02:06 +0200

    create CostItem folder with the same component & add style to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import CostItem from "./components/CostItem";
 
 function App() {
-  return <h1>Expenses App</h1>;
+  return (
+    <>
+      <h1>Expenses App</h1>
+      <CostItem />
+    </>
+  );
 }
 
 export default App;
