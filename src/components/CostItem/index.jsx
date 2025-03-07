7eb5710f37a79df93168758100e83d0f16be1c84
frontdev24kv <kv.frontdev24@gmail.com>
2025-03-07 21:03:40 +0200

    create CostItem folder with the same component & add style to it

---
+++ src/components/CostItem/index.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+const CostItem = () => {
+  return (
+    <div className='cost-item'>
+      <div>March 2021 12</div>
+      <div className='cost-item__description'>
+        <h2>fridge</h2>
+        <div className='cost-item__price'>$999.99</div>
+      </div>
+    </div>
+  )
+};
+
+export default CostItem;
